2b562bff3c571ece744ee5b0aed19052f57951db
arsh <arshtandon121@gmail.com>
2022-03-27 18:43:30 +0530

    master

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "../Intro/Intro.css";
-import face from "../img/face.png"
+import face from "../img/face.png";
 
 const Intro = () => {
   return (
     <div className="i">
       <div className="i-left">
         <div className="i-l-wrapper">
-          <h2 className="i-intro">Hello everyone ,  My Name Is </h2>
+          <h2 className="i-intro">Hello everyone , My Name Is </h2>
           <h1 className="i-name"> Arsh Deep</h1>
           <div className="i-title">
             <div className="i-title-wrapper">
@@ -17,15 +17,17 @@ const Intro = () => {
               <div className="i-title-item">MongoDB</div>
               <div className="i-title-item">angular</div>
             </div>
-           
           </div>
           <div className="i-desc">
-           <b>i am a Fresher and want to become a good web designer  ,i want to be 
-            a better version of myself , i am looking for intership </b> 
+            <b>
+              i  am a Fresher and want to become a good web designer ,i want to
+              be a better version of myself , i am looking for intership{" "}
+            </b>
           </div>
         </div>
       </div>
-      <div className="i-right"> 
+      <div className="i-right">
+        <div className="i-bg"></div>
         <img src={face} alt="" className="i-img" />
       </div>
     </div>
